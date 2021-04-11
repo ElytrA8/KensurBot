7349e22069e08bbb9be7342bb5ee2ca575954d9c
just6chill <54991398+just6chill@users.noreply.github.com>
2021-04-11 11:05:59 +0200

    Update test.py

--- userbot/modules/test.py
+++ userbot/modules/test.py
@@ -2,7 +2,7 @@ from userbot import CMD_HELP, bot
 from userbot.events import register
 
 
-@register(outgoing=True, pattern=r"^\.setbio (.*)")
+@register(outgoing=True, pattern=r"^\.test (.*)")
 
 async def test(event):
 	await event.edit("this is a test command");
